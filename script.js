5e15cc2153ad4be75dea37eddc33b8ef2ff94613
JorgeFreire95 <jorgefreire2403@hotmail.com>
2025-08-28 20:14:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(e){
+  e.preventDefault();
+  alert("¡Gracias por contactarme! Te responderé pronto.");
+  this.reset();
+});
